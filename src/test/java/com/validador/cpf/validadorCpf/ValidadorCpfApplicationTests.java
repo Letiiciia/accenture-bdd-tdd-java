daa54b1397cdb3e32048e066c1c039810a7ab3b2
letiiciia <leticialimaferreira2@gmail.com>
2021-03-20 14:52:55 -0300

    validação de mensagem de cpf valido e invalido

--- src/test/java/com/validador/cpf/validadorCpf/ValidadorCpfApplicationTests.java
+++ src/test/java/com/validador/cpf/validadorCpf/ValidadorCpfApplicationTests.java
@@ -2,117 +2,84 @@ package com.validador.cpf.validadorCpf;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-import java.util.InputMismatchException;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.validador.cpf.validadorCpf.models.Cliente;
 
 @SpringBootTest
 class ValidadorCpfApplicationTests {
 
 	@Test
 	void fazendoTesteDeCpfValido() {
-		assertEquals(true, isCPF("22713965063"));	
+		Cliente cliente = new Cliente();
+		cliente.setNome("Danilo");
+		cliente.setCpf("22713965063");
+		assertEquals(true, cliente.validarCPF());	
 	}
 
 	@Test
 	void fazendoTesteDeCpfInvalido() {
-		assertEquals(false, isCPF("22713965011"));	
+		Cliente cliente = new Cliente();
+		cliente.setNome("Danilo");
+		cliente.setCpf("22713965060");
+		assertEquals(false, cliente.validarCPF());	
 	}
 
 	@Test
 	void fazendoTesteDeCpfComDigitoAMenos() {
-		assertEquals(false, isCPF("227139650"));	
+		Cliente cliente = new Cliente();
+		cliente.setNome("Danilo");
+		cliente.setCpf("227139650");
+		assertEquals(false, cliente.validarCPF());	
 	}
 
 	@Test
 	void fazendoTesteDeCpfValidoComPontos() {
-		assertEquals(true, isCPF("227.139.650-63"));	
+		Cliente cliente = new Cliente();
+		cliente.setNome("Danilo");
+		cliente.setCpf("227.139.650-63");
+		assertEquals(true, cliente.validarCPF());	
 	}
 
 	@Test
 	void fazendoTesteDeCpfInvalidoComPontos() {
-		assertEquals(false, isCPF("227.139.65-63"));	
+		Cliente cliente = new Cliente();
+		cliente.setNome("Danilo");
+		cliente.setCpf("227.139.65-63");
+		assertEquals(false, cliente.validarCPF());	
 	}
 
 	@Test
 	void fazendoTesteDeCpfInvalidoComVirgula() {
-		assertEquals(false, isCPF("946,220.360-11"));
+		Cliente cliente = new Cliente();
+		cliente.setNome("Danilo");
+		cliente.setCpf("946,220.360-11");
+		assertEquals(false, cliente.validarCPF());
 	}
 
 	@Test
 	void fazendoTesteDeCpfInvalidoComEspacoNoFinal() {
-		assertEquals(true, isCPF("946.220.360-10 "));
+		Cliente cliente = new Cliente();
+		cliente.setNome("Danilo");
+		cliente.setCpf("946.220.360-10 ");
+		assertEquals(true, cliente.validarCPF());
 	}
 	
 	@Test
 	void fazendoTesteDeCpfInvalidoComEspacoNoComeco() {
-		assertEquals(true, isCPF(" 946.220.360-10"));
+		Cliente cliente = new Cliente();
+		cliente.setNome("Danilo");
+		cliente.setCpf(" 946.220.360-10");
+		assertEquals(true, cliente.validarCPF());
 	}
 	
 	@Test
 	void fazendoTesteDeCpfInvalidoComEspacoNoMeio() {
-		assertEquals(true, isCPF("946.220. 360-10"));
+		Cliente cliente = new Cliente();
+		cliente.setNome("Danilo");
+		cliente.setCpf("946.220. 360-10");
+		assertEquals(true, cliente.validarCPF());
 	}
-
-
-	public boolean isCPF(String CPF) {
-		CPF = CPF.replace(".", "").replace("-", "").replace(" ", "");
-
-        // considera-se erro CPF's formados por uma sequencia de numeros iguais
-        if (CPF.equals("00000000000") ||
-            CPF.equals("11111111111") ||
-            CPF.equals("22222222222") || CPF.equals("33333333333") ||
-            CPF.equals("44444444444") || CPF.equals("55555555555") ||
-            CPF.equals("66666666666") || CPF.equals("77777777777") ||
-            CPF.equals("88888888888") || CPF.equals("99999999999") ||
-            (CPF.length() != 11))
-            return(false);
-
-        char dig10, dig11;
-        int sm, i, r, num, peso;
-
-        // "try" - protege o codigo para eventuais erros de conversao de tipo (int)
-        try {
-        // Calculo do 1o. Digito Verificador
-            sm = 0;
-            peso = 10;
-            for (i=0; i<9; i++) {
-        // converte o i-esimo caractere do CPF em um numero:
-        // por exemplo, transforma o caractere '0' no inteiro 0
-        // (48 eh a posicao de '0' na tabela ASCII)
-            num = (int)(CPF.charAt(i) - 48);
-            sm = sm + (num * peso);
-            peso = peso - 1;
-            }
-
-            r = 11 - (sm % 11);
-            if ((r == 10) || (r == 11))
-                dig10 = '0';
-            else dig10 = (char)(r + 48); // converte no respectivo caractere numerico
-
-        // Calculo do 2o. Digito Verificador
-            sm = 0;
-            peso = 11;
-            for(i=0; i<10; i++) {
-            num = (int)(CPF.charAt(i) - 48);
-            sm = sm + (num * peso);
-            peso = peso - 1;
-            }
-
-            r = 11 - (sm % 11);
-            if ((r == 10) || (r == 11))
-                 dig11 = '0';
-            else dig11 = (char)(r + 48);
-
-        // Verifica se os digitos calculados conferem com os digitos informados.
-            if ((dig10 == CPF.charAt(9)) && (dig11 == CPF.charAt(10)))
-                 return(true);
-            else return(false);
-                } catch (InputMismatchException erro) {
-                return(false);
-            }
-        }
-
+	
 }
